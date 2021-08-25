505960123e9f1a57a325ac38a1c1f1070f23a322
Alex Kolyago <51213648+AlexKolyago@users.noreply.github.com>
2021-08-25 13:50:40 +0300

    Update task3.sh

--- task3.sh
+++ task3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-TIMESTUMP=$(date "+%H:%M:%S %d.%m.%Y")
+TIMESTAMP=$(date "+%H:%M:%S %d.%m.%Y")
 
 find /opt/hellthrash/images/ -type f -name "*.jpeg" -cmin -60 -size +400k > /opt/hellthrash/periodic/run_$TIMESTAMP.txt
